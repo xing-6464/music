9049c965a3d116bbe46cefa093eacdfaf02a1afd
xing-6464 <xing_6464@foxmail.com>
2022-11-25 20:06:14 +0800

    更新

--- src/service/base.js
+++ src/service/base.js
@@ -4,7 +4,7 @@ import axiosRetry from 'axios-retry'
 axiosRetry(axios, { retries: 5 })
 
 const ERR_OK = 0
-const baseURL = process.env.NODE_ENV === 'production' ? 'http://81.68.237.218/music' : '/'
+const baseURL = process.env.NODE_ENV === 'production' ? 'http://81.68.237.218/music/' : '/'
 
 axios.defaults.baseURL = baseURL
 
